f94f572603597c66bdd1eb8dd6ac98a7cd9c9da7
knoxxs_linux <knoxxs@hotmail.com>
2012-12-14 10:06:06 +0530

    Change llog functions, added debugp and logp, Increased the structure and modularity of supervisor, done error handling of supervisor except signalHandlers

--- server/accessories.h
+++ server/accessories.h
@@ -19,11 +19,18 @@
 #include <stropts.h>
 
 #define PORT "4444" //port we are listening on
-
 #define PASSCHECK "password"
+#define LOG_TYPE   1 
+#define WARN_TYPE  2 
+#define ERROR_TYPE 3 
+#define DEBUG_TYPE 4
 
 
+int logfile;
+void setLogFile(int);
 int sendall(int fd, char *buf, int *len);
 int recvall(int fd, char *buf, int *len);
-void logp(int typ, char* msg);
+void logWrite(int typ, char* msg);
 void errorp(char *where, int boolean, int errn,char *what);
+void logp(char *where, int boolean, int errn,char *what);
+void debugp(char *where, int boolean, int errn,char *what);
--- server/supervisor.c
+++ server/supervisor.c
@@ -1,55 +1,174 @@
 #include "accessories.h"
 
+#define LOG_PATH "/home/abhi/Projects/bridge/server/log"
+#define CUR_WORKING_DIREC "/home/abhi/Projects/bridge/server"
+//TODO different computer working directory issue
 
 void signalHandlerChild(int signal);
 void signalHandlerSuper(int signal);
+void makeDaemon(int);
+void initSignalHandlers(struct sigaction*, struct sigaction*);
+
 
 int main() { 
+	int logfile;
 
+	setLogFile(STDOUT_FILENO);
+	logp("Supervisor- Main", 0,0 ,"Starting");
+	if( (logfile = open(LOG_PATH, O_RDWR|O_CREAT|O_APPEND,S_IRUSR|S_IWUSR|S_IRGRP|S_IWGRP)) < 0 ){
+		errorp("Supervisor -Main",0,0,"Error Opening logfile");
+		debugp("Supervisor -Main",1,1,NULL);
+	}
+	setLogFile(logfile);
+	logp("________________________",0,0,"________________________");
+
+	logp("Supervisor- Main",0,0,"Logfile opened Succesfuly");
+
+
+
+	logp("Supervisor- Main",0,0,"Making Daemon");
+	makeDaemon(logfile);
+	logp("Supervisor- Main",0,0,"Daemon Made Succesfuly");
+
+
+	pid_t supervisor_pid, dispactcher_pid;
+
+	//signal struct
+	static struct sigaction sigact_struct_child, sigact_struct_super;
+	
+	logp("Supervisor- Main",0,0,"Starting Initialising Signal Handlers");
+	initSignalHandlers(&sigact_struct_child, &sigact_struct_super);
+	logp("Supervisor- Main",0,0,"Signal Initialised Succesfuly");
+
+	//TODO pid int to string
+	supervisor_pid = getpid();
+	logp("Supervisor- Main",0,0,"Supervisor Pid is ");
+
+
+	//Starting Dispatcher
+	logp("Supervisor- Main",0,0,"Forking For Starting dispatcher");
+	dispactcher_pid = fork();
+	if(dispactcher_pid < 0){ //parent
+		errorp("Supervisor-Main",0,0,"Forking For Dispatcher");
+    	debugp("Supervisor-Main",1,errno,NULL);
+		_exit(127);
+	}
+	if(dispactcher_pid == 0){ //child
+		logp("Supervisor- Main",0,0,"Executing Dispatcher");
+		execl("./bin/dispatcher",(const char*) NULL,(char *) 0); // need to change this to exec + e - controll environment
+		//only returns when error
+		errorp("Supervisor-Main",0,0,"Executing Dispatcher");
+    	debugp("Supervisor-Main",1,errno,NULL);
+		_exit(127);
+	}
+
+	logp("Supervisor- Main",0,0,"Entering Wait Loop");
+	while(1){}
+}
+
+void initSignalHandlers(struct sigaction* sigact_struct_child,struct sigaction* sigact_struct_super){
+	
+	logp("Supervisor- initSignalHandlers",0,0,"Filling signal structs");
+	sigfillset(&(sigact_struct_child->sa_mask));//add all the signals
+	sigfillset(&(sigact_struct_super->sa_mask));//add all the signals
+
+	logp("Supervisor- initSignalHandlers",0,0,"Setting Handlers of structs");
+	sigact_struct_child->sa_handler = signalHandlerChild;
+	sigact_struct_super->sa_handler = signalHandlerSuper;
+
+	logp("Supervisor- initSignalHandlers",0,0,"Setting Flags");
+	sigact_struct_child->sa_flags |= SA_RESTART;
+	sigact_struct_super->sa_flags |= SA_RESTART;
+
+	logp("Supervisor- initSignalHandlers",0,0,"Registering Child");
+	//handling signal from child
+	sigaction(SIGCHLD, sigact_struct_child, NULL);
+
+	logp("Supervisor- initSignalHandlers",0,0,"Starting Registering Super");
+	//signal handling for super process
+	sigaction(SIGHUP,  sigact_struct_super, NULL); //hangup detected          /term    /1
+	sigaction(SIGINT,  sigact_struct_super, NULL); //Interrupt from keyboard  /term    /2
+	sigaction(SIGQUIT, sigact_struct_super, NULL); //Quit from keyboard      /core    /3
+	sigaction(SIGILL,  sigact_struct_super, NULL); //Illegal Instruction      /core    /4
+	sigaction(SIGABRT, sigact_struct_super, NULL); //Abort signal from abort /core    /6
+	sigaction(SIGFPE,  sigact_struct_super, NULL); //Floating Pt. Exception  /core    /8
+	//sigaction(SIGKILL, &sigact_struct_super, NULL); //Kill Signal           /term    /9
+	sigaction(SIGSEGV, sigact_struct_super, NULL); //Invalid memory reference/core    /11
+	sigaction(SIGPIPE, sigact_struct_super, NULL); //Broken Pipe: write to a pipe with no reader   /term   /13
+	sigaction(SIGALRM, sigact_struct_super, NULL); //Timer signal from alarm /term    /14
+	sigaction(SIGTERM, sigact_struct_super, NULL); //Termination signal      /term    /15
+	sigaction(SIGUSR1, sigact_struct_super, NULL); //user defined signal 1   /term    /30,10,16
+	sigaction(SIGUSR2, sigact_struct_super, NULL); //user defined signal 2   /term    /31,12,17
+	//sigaction(SIGSTOP, &sigact_struct_super, NULL); //stop process          /stop    /17,19,23
+	sigaction(SIGTSTP, sigact_struct_super, NULL); //stop typed at tty       /stop    /18,20,24
+	sigaction(SIGTTIN, sigact_struct_super, NULL); //tty input from bg proc  /stop    /21,21,26
+	sigaction(SIGTTOU, sigact_struct_super, NULL); //tty output from bg proc /stop    /22,22,27
+	sigaction(SIGBUS,  sigact_struct_super, NULL); //bus error(bad mem access)/core    /10,7,10
+	sigaction(SIGPROF, sigact_struct_super, NULL); //profiling time expired  /term    /27,27,29
+	sigaction(SIGTRAP, sigact_struct_super, NULL); //trace/breakpoint trap   /core    /5
+	sigaction(SIGVTALRM, sigact_struct_super, NULL); //virtual alrm clock    /term    /26,26,28
+	sigaction(SIGXCPU, sigact_struct_super, NULL); //cpu time limit exceeded /core    /24,24,30
+	sigaction(SIGXFSZ, sigact_struct_super, NULL); //file size limit exceeded/core    /25,25,31
+	sigaction(SIGIOT,  sigact_struct_super, NULL); //IOT trap				  /core    /6
+	logp("Supervisor- initSignalHandlers",0,0,"Done Registering Super");
+}
+
+void makeDaemon(int logfile){
 	pid_t pidToDaemon, retPid;
 
-
+	logp("Supervisor-makeDaemon",0,0,"Forking First Child");
 	/* Fork off the parent process */
     pidToDaemon = fork();
     if (pidToDaemon < 0) {
-            exit(EXIT_FAILURE);
+    	errorp("Supervisor-makeDaemon",0,0,"Error While Forking First Child");
+    	debugp("Supervisor-makeDaemon",1,errno,NULL);
+        exit(EXIT_FAILURE);
     }
     if (pidToDaemon > 0) {
-            exit(EXIT_SUCCESS); 
+    	logp("Supervisor-makeDaemon",0,0,"Exiting First Parent");
+        exit(EXIT_SUCCESS); 
     }
-  
-	/* -----------child (daemon) continues ----------------*/
 
+	/* -----------child (daemon) continues ----------------*/
+    logp("Supervisor-makeDaemon",0,0,"First Child Continues and adding SIGHUP Handler");
     signal(SIGHUP,SIG_IGN);
+
 	/* Fork off the SECOND parent process */
+	logp("Supervisor-makeDaemon",0,0,"Forking Second Child");
     pidToDaemon = fork();
     if (pidToDaemon < 0) {
-            exit(EXIT_FAILURE);
+    	errorp("Supervisor-makeDaemon",0,0,"Error While Forking Second Child");
+    	debugp("Supervisor-makeDaemon",1,errno,NULL);
+        exit(EXIT_FAILURE);
     }
     if (pidToDaemon > 0) {
-            exit(EXIT_SUCCESS); 
+    	logp("Supervisor-makeDaemon",0,0,"Exiting Second Parent");
+        exit(EXIT_SUCCESS); 
     }
 
     /* -----------SECOND child (daemon) continues ----------------*/
+    logp("Supervisor-makeDaemon",0,0,"Second Child Continues and Creating New Session");
 
 	/* Create a new SID for the child process */
 	retPid = setsid();
 	if (retPid < 0) {
-    	 /* Log any failure */
+    	errorp("Supervisor-makeDaemon",0,0,"Creating New Session");
+    	debugp("Supervisor-makeDaemon",1,errno,NULL);
      	exit(EXIT_FAILURE);
 	}
 
-
+	logp("Supervisor-makeDaemon",0,0,"Changing umask");
 	/* Change the file mode mask */
 	umask(027);  //can also use umask(0), but this is better
 
-	// Closing standard file descriptors ????????????????
+	// Closing standard file descriptors 
+	logp("Supervisor-makeDaemon",0,0,"Closing STDIN");
 	close(STDIN_FILENO);
-	if (open("/dev/null",O_RDONLY) == -1) {
-		//die("failed to reopen stdin while daemonising (errno=%d)",errno);
-	}
+	// if (open("/dev/null",O_RDONLY) == -1) {
+	// 	//die("failed to reopen stdin while daemonising (errno=%d)",errno);
+	// }
 
-	//no log file------
+	//uncomment when no log file------
 	/*
 	close(STDOUT_FILENO);
 	if (open("/dev/null",O_WRONLY) == -1) {
@@ -62,105 +181,20 @@ int main() {
 	*/
 
 	//with log file
-	int logfile_fileno = open("/home/abhi/Projects/bridge/SuperLog/log",O_RDWR|O_CREAT|O_APPEND,S_IRUSR|S_IWUSR|S_IRGRP|S_IWGRP);
-	if (logfile_fileno == -1) {
-    	//die("failed to open logfile (errno=%d)",errno);
-	}
-
-	dup2(logfile_fileno,STDOUT_FILENO);
-	dup2(logfile_fileno,STDERR_FILENO);
+	logp("Supervisor-makeDaemon",0,0,"Copying STDOUT/ERR to logfile");
+	dup2(logfile,STDOUT_FILENO);
+	dup2(logfile,STDERR_FILENO);
 	
-	write(logfile_fileno, "abhishek1\n",11);
-
-//	close(logfile_fileno);
-
 
 	/* Change the current working directory */
 	//cant use short path "~/bridge/server"
-	if ((chdir("/home/abhi/Projects/bridge/server")) < 0) {
-        /* Log any failure here */
-        errorp("Supervisor- To check FORKING", 1, errno,"UYYYYYY0");
+	logp("Supervisor-makeDaemon",0,0,"Changing Current Working Directory");
+	if ((chdir(CUR_WORKING_DIREC)) < 0) {
+		errorp("Supervisor-makeDaemon",0,0,"Changing Directory");
+        debugp("Supervisor- To check FORKING", 1, errno,NULL);
         exit(EXIT_FAILURE);
 	}
-
-	write(logfile_fileno, "abhishek2\n",11);
-
-	pid_t supervisor_pid, dispactcher_pid;
-	int dispatcher_status;
-
-	//signal struct
-	static struct sigaction sigact_struct_child, sigact_struct_super;
-	sigfillset(&(sigact_struct_child.sa_mask));//add all the signals
-	sigfillset(&(sigact_struct_super.sa_mask));//add all the signals
-
-	sigact_struct_child.sa_handler = signalHandlerChild;
-	sigact_struct_super.sa_handler = signalHandlerSuper;
-
-	sigact_struct_child.sa_flags |= SA_RESTART;
-	sigact_struct_super.sa_flags |= SA_RESTART;
-
-	//handling signal from child
-	sigaction(SIGCHLD, &sigact_struct_child, NULL);
-
-	write(logfile_fileno, "abhishek3\n",11);
-
-	//signal handling for super process
-	sigaction(SIGHUP, &sigact_struct_super, NULL); //hangup detected          /term    /1
-	sigaction(SIGINT, &sigact_struct_super, NULL); //Interrupt from keyboard  /term    /2
-	sigaction(SIGQUIT, &sigact_struct_super, NULL); //Quit from keyboard      /core    /3
-	sigaction(SIGILL, &sigact_struct_super, NULL); //Illegal Instruction      /core    /4
-	sigaction(SIGABRT, &sigact_struct_super, NULL); //Abort signal from abort /core    /6
-	sigaction(SIGFPE, &sigact_struct_super, NULL); //Floating Pt. Exception  /core    /8
-	//sigaction(SIGKILL, &sigact_struct_super, NULL); //Kill Signal           /term    /9
-	sigaction(SIGSEGV, &sigact_struct_super, NULL); //Invalid memory reference/core    /11
-	sigaction(SIGPIPE, &sigact_struct_super, NULL); //Broken Pipe: write to a pipe with no reader   /term   /13
-	sigaction(SIGALRM, &sigact_struct_super, NULL); //Timer signal from alarm /term    /14
-	sigaction(SIGTERM, &sigact_struct_super, NULL); //Termination signal      /term    /15
-	sigaction(SIGUSR1, &sigact_struct_super, NULL); //user defined signal 1   /term    /30,10,16
-	sigaction(SIGUSR2, &sigact_struct_super, NULL); //user defined signal 2   /term    /31,12,17
-	//sigaction(SIGSTOP, &sigact_struct_super, NULL); //stop process          /stop    /17,19,23
-	sigaction(SIGTSTP, &sigact_struct_super, NULL); //stop typed at tty       /stop    /18,20,24
-	sigaction(SIGTTIN, &sigact_struct_super, NULL); //tty input from bg proc  /stop    /21,21,26
-	sigaction(SIGTTOU, &sigact_struct_super, NULL); //tty output from bg proc /stop    /22,22,27
-	sigaction(SIGBUS, &sigact_struct_super, NULL); //bus error(bad mem access)/core    /10,7,10
-	sigaction(SIGPROF, &sigact_struct_super, NULL); //profiling time expired  /term    /27,27,29
-	sigaction(SIGTRAP, &sigact_struct_super, NULL); //trace/breakpoint trap   /core    /5
-	sigaction(SIGVTALRM, &sigact_struct_super, NULL); //virtual alrm clock    /term    /26,26,28
-	sigaction(SIGXCPU, &sigact_struct_super, NULL); //cpu time limit exceeded /core    /24,24,30
-	sigaction(SIGXFSZ, &sigact_struct_super, NULL); //file size limit exceeded/core    /25,25,31
-	sigaction(SIGIOT, &sigact_struct_super, NULL); //IOT trap				  /core    /6
-	
-
-
-	supervisor_pid = getpid();
-
-	write(logfile_fileno, "abhishek4\n",11);
-
-	switch( (dispactcher_pid = fork()) )
-	{
-		case -1:
-			{
-				//error starting dispatcher child
-				dispatcher_status = -1;
-			}
-		case 0: //dipatcher child
-			{
-					write(logfile_fileno, "abhishek5\n",11);
-				printf("child %d\n",dispactcher_pid);
-				execl("./bin/dispatcher",(const char*) NULL,(char *) 0); // need to change this to exec + e - controll environment
-				_exit(127);
-			}
-	}
-
-
-
-	int i;
-	while(1)
-	{}
-//	for(i=0;i<1000000;i++);
-//	printf("%d\n",supervisor_pid);
 }
-
 
 void signalHandlerChild(int signal){
 	int status, return_val;
@@ -195,7 +229,7 @@ void signalHandlerSuper(int signal)
 			scanf("%s",pass);
 			strcpy(log_pass,temp);
 			strcat(log_pass, pass);
-			logp(4, log_pass);
+			logWrite(4, log_pass);
 			if( strcmp(pass, PASSCHECK) == 0)
 			{
 				errorp("Supervisor- signalHandlerSuper in SIGINT", 0, 0,"User entered correct pass - so QUITING");
@@ -217,7 +251,7 @@ void signalHandlerSuper(int signal)
 			scanf("%s",pass);
 			strcpy(log_pass,temp);
 			strcat(log_pass, pass);
-			logp( 4 , log_pass);
+			logWrite( 4 , log_pass);
 			if( pass == PASSCHECK)
 			{
 				errorp("Supervisor- signalHandlerSuper in SIGQUIT", 0, 0,"User entered correct pass - so QUITING");
