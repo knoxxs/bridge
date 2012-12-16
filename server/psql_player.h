66ec76f1ae19c089730db3050ab697e4f1bfb5af
knoxxs_linux <knoxxs@hotmail.com>
2012-12-16 21:22:17 +0530

    Removed psql_player.*

--- server/psql_player.h
+++
@@ -1,7 +0,0 @@
-#include <libpq-fe.h>
-#include <string>
-using namespace std;
-
-PGconn *ConnectDB(string ,string, string, string, string);
-int getPlayerInfoFromDb(PGconn *, string, char*, char*);
-void CloseConn(PGconn*);
