1efcf5d0f7ea4b3411d5c585793f0b30a75f89b8
knoxxs_linux <knoxxs@hotmail.com>
2012-12-28 23:15:27 +0530

    done definition of class trcik tricks player

--- server/game.h
+++ server/game.h
@@ -26,10 +26,12 @@ ssize_t errcheckfunc(int, const void *, size_t);
 void* gameMain(void*);
 void connection_handler(int);
 int getPlayerInfo(char *, char *, char *, int, int, int, int);
+char nextPos(char);
 
 class Card{
 	public:
-		Card(char, char);
+		Card(char, char, bool);
+		Card();
 		string print();
 		char getSuit();
 		char getRank();
@@ -48,33 +50,41 @@ class Deck{
 	private:
 		vector<Card> deck;
 };
+
 class Trick{
 	public:
 		Trick(char);
+		Trick();
 		string print();
 		Card nextCard(int);
 		void nextCard(Card);
+		void setWinner();
+		char getWinner();
 		int score();
 	private:
 		Card cards[4];
 		char first;
+		int i;
+		char winner;
 };
+
 class Tricks{
 	public:
 		Tricks();
 		string print();
 		void add(Trick);
 		Trick get(int);
+		void setWinner();
 		char getWinner();
 		int score(char);
 	private:
 		Trick tricks[13];
-		char winner[13];
 		int index;
+		char winner;
 };
 class Player{
 	public:
-		Player();
+		Player(string, char, char, string, string, string);
 		int getUserChoice();
 	private:
 		char position;
@@ -84,11 +94,11 @@ class Player{
 };
 class Team{
 	public:
+		Team(char, string,string, string);
 		int score();
 	private:
 		char team;
-		string plid1, plid2;
-
+		string tid, plid1, plid2;
 };
 
 #endif
